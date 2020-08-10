7c16dbfe270c46b3bbf0f37d1d1f09cf6c435cc3
navayuvan-sb <ns.navayuvan@gmail.com>
2020-08-10 17:10:46 +0530

    [IMP] AUTH: Automatic Routing Login - defined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,28 +15,52 @@ import firebaseConfig from "../credentials/firebase";
 
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
 import { DoneTaskComponent } from "../components/done-task/done-task";
-import { ThemeProvider } from '../providers/theme/theme';
+import { ThemeProvider } from "../providers/theme/theme";
 
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from "@ionic/storage";
+import { AuthProvider } from "../providers/auth/auth";
+import { LoginPage } from "../pages/login/login";
+import { ResponseViewsProvider } from "../providers/response-views/response-views";
+import { SignupPage } from "../pages/signup/signup";
 
 @NgModule({
-  declarations: [MyApp, HomePage, DonePage, UnDonePage, UndoneTaskComponent, DoneTaskComponent],
+  declarations: [
+    MyApp,
+    LoginPage,
+    SignupPage,
+    HomePage,
+    DonePage,
+    UnDonePage,
+    UndoneTaskComponent,
+    DoneTaskComponent,
+  ],
   imports: [
     BrowserModule,
     AngularFirestoreModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp),
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, HomePage, DonePage, UnDonePage],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    HomePage,
+    SignupPage,
+    DonePage,
+    UnDonePage,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     TaskProvider,
     ThemeProvider,
+    AuthProvider,
+    ResponseViewsProvider,
   ],
 })
 export class AppModule {}
